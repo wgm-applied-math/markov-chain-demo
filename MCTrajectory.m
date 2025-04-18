15907e7143495d941af74b07feb8327ecd992a9b
W. Garrett Mitchener <garrett.mitchener@gmail.com>
2025-04-18 14:31:54 -0400

    Use isapprox rather than == 1
    because == 1.0 is potentially going to fail due to precision errors

--- MCTrajectory.m
+++ MCTrajectory.m
@@ -9,7 +9,7 @@ N = size(P, 1);
 % Check that the transition matrix is proper.
 assert(N == size(P, 2), "The matrix P must be square");
 assert(min(P, [], "all") >= 0, "All entries of P must be >= 0");
-assert(all(sum(P, 2) == 1), "All rows must sum to 1");
+assert(all(isapprox(sum(P, 2), 1, "veryloose")), "All rows must sum to 1");
 assert(1 <= StartState && StartState <= N, ...
     "The start state must be between 1 and the size of P");
 
